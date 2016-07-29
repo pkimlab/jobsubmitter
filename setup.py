1729259135879f1603a8660912c9a5ce69f931f4
ostrokach <ostrokach@gmail.com>
2016-07-29 14:52:48 -0400

    "Release 0.0.2"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jobsubmitter',
-    version='0.0.1',
+    version='0.0.2',
     author='kimlab.org',
     packages=find_packages(),
     package_data={},
