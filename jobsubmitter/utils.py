937fc83a97303ce9c629a7427392dc168925520e
ostrokach <ostrokach@gmail.com>
2017-11-08 21:53:44 -0500

    Extract execute_remotely into a standalone function

--- jobsubmitter/utils.py
+++ jobsubmitter/utils.py
@@ -1,32 +1,28 @@
-def iterate_parameters(global_params=None, **parameter_grid):
-    """.
+import logging
 
-    Parameters
-    ----------
-    parameter_grid : dict
-        Keys are parameters. Values are lists of values that the parameters can take.
+import paramiko
 
-    Examples
-    --------
-    >>> from pprint import pprint
-    >>> iterable = iterate_parameters(a=[1, 2], b=[3, 4])
-    >>> pprint(sorted(iterable, key=lambda x: (x['a'], x['b'])))
-    [{'a': 1, 'b': 3}, {'a': 1, 'b': 4}, {'a': 2, 'b': 3}, {'a': 2, 'b': 4}]
+from kmtools import system_tools
+
+logger = logging.getLogger(__name__)
+
+
+@system_tools.retry_ssh
+def execute_remotely(system_command: str, ssh: paramiko.SSHClient) -> str:
+    """Execute a system command on a remote server.
+
+    Returns:
+        STDOUT from the remote execution.
+        We do not return STDERR because we treat the presence of STDERR as an error
+        (this is why this function is not very generalizable).
     """
-    # Don't modify dictionaries in-place
-    if global_params is None:
-        params = dict()
-    else:
-        params = global_params.copy()
-    # Terminal case
-    if not parameter_grid:
-        yield params
-        return
-    # Recurse
-    key, values = parameter_grid.popitem()
-    for value in values:
-        params[key] = value
-        try:
-            yield from iterate_parameters(params.copy(), **parameter_grid)
-        except StopIteration:
-            continue
+    logger.debug("system_command: '%s'", system_command)
+    stdin_fh, stdout_fh, stderr_fh = ssh.exec_command(system_command, get_pty=True)
+    stdout = stdout_fh.read().decode().strip()
+    stderr = stderr_fh.read().decode().strip()
+    if stdout:
+        logger.debug(stdout)
+    if stderr:
+        logger.warning(stderr)
+        raise paramiko.ChannelException(0, stderr)
+    return stdout
