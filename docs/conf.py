e30c1736729f5870f1edb473247bf5ac56febe6c
ostrokach <ostrokach@gmail.com>
2018-02-22 13:57:10 -0500

    Set sphinx napoleon options

--- docs/conf.py
+++ docs/conf.py
@@ -295,3 +295,16 @@ texinfo_documents = [
 
 autosummary_generate = True
 locales = 'en'
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_rtype = True
